4bad9d68ac5404545a95daaf7feda7535f493696
tonymputhia <kimathitony93@gmail.com>
2018-08-27 17:11:49 +0300

    [Feature #159815147] Update routes to include login and sign up

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
-import LoginPage from './components/home/LoginPage';
-import SignupPage from './components/home/SignupPage';
+import LoginPage from './components/login/LoginPage';
+import SignupPage from './components/signup/SignupPage';
 
 export default (
     <Route path="/" component={App}>
