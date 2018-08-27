217d50d4752d9628a5999eaa7977ec6e5dcb0c19
tonymputhia <kimathitony93@gmail.com>
2018-08-27 17:30:28 +0300

    [Feature #159815147] Modify Components

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Header from './common/Header';
+import Footer from './common/Footer';
 
 class App extends React.Component {
     render() {
         return (
             <div>
                 <div className="container">
-                    {this.props.children}
+                <Header />
+                {this.props.children}
+                <Footer />
                 </div>
             </div>
         );
     } 
 }
 
-App.PropTypes = {
+App.propTypes = {
     children: PropTypes.object.isRequired
 };
 
