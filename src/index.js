c4ff502e3d990a3827405d3ddfc10e1860858099
tonymputhia <kimathitony93@gmail.com>
2018-08-28 10:26:31 +0300

    [Feature #159815147] Attach Store to Components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/configureStore';
+import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import './styles/style.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import registerServiceWorker from './registerServiceWorker';
 
+const store = configureStore();
+
 ReactDOM.render(
-    <Router history={browserHistory} routes={routes} />, 
+    <Provider store={store}>
+        <Router history={browserHistory} routes={routes} />
+    </Provider>, 
     document.getElementById('root'));
 registerServiceWorker();
