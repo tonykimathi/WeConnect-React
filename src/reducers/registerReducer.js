a4818434557bea28ad61f237359ddbf389861cb0
tonymputhia <kimathitony93@gmail.com>
2018-08-29 12:26:23 +0300

    [Feature #159815147] Modify Register Reducer

--- src/reducers/registerReducer.js
+++ src/reducers/registerReducer.js
@@ -1,10 +1,18 @@
 import * as types from '../actions/actionTypes';
 
-export default function registerReducer(state=[], action) {
+const initialState = {
+    error: null
+  };
+
+export default function registerReducer(state=initialState, action) {
     switch (action.type) {
         case types.REGISTER_USER:
         return [...state,
-            Object.assign({}, action.user)
+            Object.assign({}, action.payload)
+        ];
+        case types.REGISTER_ERROR:
+        return [...state,
+            Object.assign({}, action.payload)
         ];
 
     default:
