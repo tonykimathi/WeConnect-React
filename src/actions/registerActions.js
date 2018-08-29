9e44d4208afb9a069d3d262ddb8d6964d19bf7c4
tonymputhia <kimathitony93@gmail.com>
2018-08-29 12:25:57 +0300

    [Feature #159815147] Modify Register Action

--- src/actions/registerActions.js
+++ src/actions/registerActions.js
@@ -1,5 +1,25 @@
 import * as types from './actionTypes';
+import axios from 'axios';
+import {api_url} from '../config';
+import {browserHistory} from 'react-router';
 
-export function registerUser(user) {
-    return { type: types.REGISTER_USER, user}
-}
+export const registerUser = data => {
+    
+    return dispatch => {
+        const register_url = `${api_url}register`;
+         axios.post(register_url, {username: data.username,
+                                  email: data.email,
+                                  password: data.password, 
+                                  confirm_password: data.confirm_password})
+        .then(res => {
+            dispatch({ type: types.REGISTER_USER, payload: res.data.message });
+            browserHistory.push('/login');
+        })
+        .catch(error => {
+            if (error.response.status === 401) {
+                dispatch({ type: types.REGISTER_ERROR, payload: error.response.data.message });
+            };
+        })
+    };
+    
+};
