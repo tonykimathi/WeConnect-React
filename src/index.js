2ef92c596e14885c01a38d26e5d6d91a1c821244
tonymputhia <kimathitony93@gmail.com>
2018-09-09 19:27:53 +0300

    [Feature #159815159] Dispatch actions to store

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/configureStore';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
+import * as businessActions from './actions/businessActions';
 import routes from './routes';
 import './styles/style.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import registerServiceWorker from './registerServiceWorker';
 
 const store = configureStore();
+store.dispatch(businessActions.getAllBusinesses(),
+               businessActions.getSingleBusiness(),
+            );
 
 ReactDOM.render(
     <Provider store={store}>
