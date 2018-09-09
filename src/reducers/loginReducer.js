663405e5b424a48c6ed971ebf0f431452cca0bb6
tonymputhia <kimathitony93@gmail.com>
2018-09-09 12:36:52 +0300

    [Feature #159815147] Modify auth reducers

--- src/reducers/loginReducer.js
+++ src/reducers/loginReducer.js
@@ -4,21 +4,29 @@ const initialState = {
     username: localStorage.getItem('username'),
     auth_token: localStorage.getItem('auth_token'),
     email: localStorage.getItem('email'),
-    error: null
+    loggedIn: JSON.parse(localStorage.getItem('loggedIn')),
+    error: null,
+    message: ''
   };
 
 export default function loginReducer(state=initialState, action) {
     switch (action.type) {
         case types.LOGIN_USER:
-        return [...state,
-            Object.assign({}, action.payload)
-        ];
+        return {...state,
+            username: action.payload.username,
+            auth_token: action.payload.auth_token,
+            email: action.data.email,
+            message: 'Login Success',
+            loggedIn: true
+        };
         case types.LOGIN_ERROR:
-        return [...state,
-            Object.assign({}, action.payload)
-        ]; 
+        return {...state,
+            message: action.payload,
+            loggedIn: false,
+            error: true
+        };
 
     default:
-        return state
+        return {...state}
     }
 }
--- src/reducers/registerReducer.js
+++ src/reducers/registerReducer.js
@@ -1,21 +1,23 @@
 import * as types from '../actions/actionTypes';
 
 const initialState = {
-    error: null
+    error: null,
+    message: ''
   };
 
 export default function registerReducer(state=initialState, action) {
     switch (action.type) {
         case types.REGISTER_USER:
-        return [...state,
-            Object.assign({}, action.payload)
-        ];
+        return {...state,
+            message: 'Register Success'
+        };
         case types.REGISTER_ERROR:
-        return [...state,
-            Object.assign({}, action.payload)
-        ];
+        return {...state,
+            message: action.payload.error,
+            error: true
+        };
 
     default:
-        return state
+        return {...state}
     }
 }
