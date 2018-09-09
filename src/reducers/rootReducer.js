8cb1c8d7d497027332536b2c2e2f5aa3376e233e
tonymputhia <kimathitony93@gmail.com>
2018-09-09 19:28:27 +0300

    [Feature #159815159] Add business reducers to root reducers

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,11 +2,16 @@ import {combineReducers} from 'redux';
 import Register from './registerReducer';
 import Login from './loginReducer';
 import Logout from './logoutReducer';
+import Business from './businessReducer';
+import {fetchBusinesses, fetchSingleBusiness} from './fetchBusinessReducer';
 
 const rootReducer = combineReducers({
     Register,
     Login,
-    Logout
+    Logout,
+    Business,
+    fetchBusinesses,
+    fetchSingleBusiness 
 });
 
 export default rootReducer;
