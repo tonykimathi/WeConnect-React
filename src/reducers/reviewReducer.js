2465b262a80d4d28f4951ccda57b2dcaa14eef68
tonymputhia <kimathitony93@gmail.com>
2018-09-12 10:20:09 +0300

    [Feature #159815163] Add indentation to improve readability

--- src/reducers/reviewReducer.js
+++ src/reducers/reviewReducer.js
@@ -15,34 +15,34 @@ const reviewsInitialState = {
 export const createReview = (state=initialState, action)  => {
     switch (action.type) {
         case types.CREATE_REVIEW:
-        return {...state,
-            message: action.payload,
-            error: false
-        };
+            return {...state,
+                message: action.payload,
+                error: false
+            };
         case types.CREATE_REVIEW_ERROR:
-        return {...state,
-            message: action.payload,
-            error: true
-        };
+            return {...state,
+                message: action.payload,
+                error: true
+            };
         default:
-        return {...state}
+            return {...state}
     }
 };
 
 export const fetchReviews = (state=reviewsInitialState, action) => {
     switch (action.type) {
         case types.GET_ALL_REVIEWS:
-        return {...state,
-            message: 'Reviews retrieved successfully',
-            businesses: action.payload.businesses,
-            error: false
-        };
+            return {...state,
+                message: 'Reviews retrieved successfully',
+                businesses: action.payload.businesses,
+                error: false
+            };
         case types.GET_ALL_REVIEWS_ERROR:
-        return {...state,
-            message: action.payload,
-            error: true
-        };
+            return {...state,
+                message: action.payload,
+                error: true
+            };
         default:
-        return {...state}
+            return {...state}
     }
 };
